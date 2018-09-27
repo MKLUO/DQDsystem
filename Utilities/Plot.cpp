8ce745f579168071144742b01f50af8bc725dada
MKLUO <kkeenn12345566@gmail.com>
2018-09-27 23:37:25 +0800

    Removed Gnugraph since it's not compatible with linux.

--- Utilities/Plot.cpp
+++ Utilities/Plot.cpp
@@ -9,24 +9,12 @@ using std::string;
 
 #include "Plot.h"
 
-#include "gnugraph/GnuGraph.h"
+//#include "gnugraph/GnuGraph.h"
 
 // TODO: implements plotting
 
 void plotter::plotTest() {
-    // TODO: this path should be determined in cmake
-    GnuGraph graph("C:/Program Files/gnuplot/bin/gnuplot.exe"); // provide path to executable
 
-    vector<double> x, y;
-    for (size_t i = 0; i < 200; ++i)
-    {
-        x.push_back(double(i));
-        y.push_back(sqrt(x[i]));
-    }
-
-    const string output = graph.plot(x, y, "y = sqrt(x)");
-    cout << output << '\n'; // print any errors to console
-    cin.get(); // keep the window open until the user presses ENTER
 }
 void plotter::plotFieldAbs(const ScalarField &field) {
 
